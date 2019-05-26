bd92f395dd82eb9e0ad0a236afa4dd56c84bf727
hiep1bg <hiep1bg@gmail.com>
2019-05-26 13:23:58 +0700

    add more api

---
+++ api/user/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { register, getAllUsers, getOneUser } = require("./controller");
+const router = express.Router();
+
+router.post("/", (req, res) => {
+    register(req.body)
+    .then(id => res.send(id))
+    .catch(err => {
+        res.status(500).send(err);
+    })
+});
+
+router.get("/", (req, res) => {
+    getAllUsers(req.query.page || 1)
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).send(err));
+});
+
+router.get("/:id", (req, res) => {
+    getOneUser(req.params.id, req.body.password)
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).send(err));
+});
+
+router.post("/login");
+
+module.exports = router;
--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const config = require('./config.json');
+const usersRouter = require("./api/user/index");
 
 var app = express();
 
@@ -20,25 +22,33 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-const port = process.env.PORT || 3000;
-app.listen(port, err => {
-  if(err) console.log(err);
-  else console.log("Successful to connect on port: "+ port);
+mongoose.connect(config.MONGO_DB, { useNewUrlParser: true }, err => {
+  if (err) console.log(err);
+  else {
+    console.log("Successful to connect mongodb");
+    app.use("/api/v1/user", usersRouter);
+    app.use(function (req, res, next) {
+      next(createError(404));
+    });
+    app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+    const port = process.env.PORT || 3000;
+    app.listen(port, err => {
+      if (err) console.log(err);
+      else console.log("Successful to connect on port: " + port);
+    })
+  }
 })
